ec1d3b8a9f11b0b3fd245c673cb687c7cb0ddf76
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2019-02-11 09:50:30 -0500

    parallelizing

--- measure_ages.py
+++ measure_ages.py
@@ -1,44 +1,45 @@
 import stardate as sd
 import pandas as pd
 import numpy as np
+from multiprocessing import Pool
 
-df = pd.read_csv("kane_cks_tdmra_dr2.csv")
+def measure_koi_ages(row):
+    df = row[1]
 
-savedir = "planet_results"
+    iso_params = {"G": (df.phot_g_mean_mag,
+                        df.phot_g_mean_mag*.05),
+                  "bp": (df.phot_bp_mean_mag,
+                         df.phot_bp_mean_mag*.05),
+                  "rp": (df.phot_rp_mean_mag,
+                         df.phot_rp_mean_mag*.05),
+                  "J": (df.kic_jmag,
+                        df.kic_jmag*.05),
+                  "H": (df.kic_hmag,
+                        df.kic_hmag*.05),
+                  "K": (df.kic_kmag,
+                        df.kic_kmag*.05),
+                  "teff": (df.iso_steff,
+                           df.iso_steff_err2),
+                  "logg": (df.iso_slogg,
+                           df.iso_slogg_err2),
+                  "feh": (df.iso_smet,
+                          df.iso_smet_err2),
+                  "parallax": (df.parallax,
+                               df.parallax_error)}
 
-prot, prot_err = df.period, df.period_errm
+    starname = str(int(df.id_kic)).zfill(9)
+    filename = "planet_results/{}".format(starname)
 
-N = len(df)
+    star = sd.Star(
+        iso_params, df.period, .5*(df.period_errp + df.period_errm),
+        filename=filename)
 
-m = np.arange(len(df.id_kic.values)) > 22
-df = df.iloc[m]
+    sampler = star.fit(max_n=200000)
 
-for i, id in enumerate(df.id_kic.values):
-    print(i, "of", N)
-    print("kepid = ", str(id).zfill(9))
 
-    iso_params = {"G": (df.phot_g_mean_mag.values[i],
-                        df.phot_g_mean_mag.values[i]*.05),
-                  "bp": (df.phot_bp_mean_mag.values[i],
-                         df.phot_bp_mean_mag.values[i]*.05),
-                  "rp": (df.phot_rp_mean_mag.values[i],
-                         df.phot_rp_mean_mag.values[i]*.05),
-                  "J": (df.kic_jmag.values[i],
-                        df.kic_jmag.values[i]*.05),
-                  "H": (df.kic_hmag.values[i],
-                        df.kic_hmag.values[i]*.05),
-                  "K": (df.kic_kmag.values[i],
-                        df.kic_kmag.values[i]*.05),
-                  "teff": (df.iso_steff.values[i],
-                           df.iso_steff_err2.values[i]),
-                  "logg": (df.iso_slogg.values[i],
-                           df.iso_slogg_err2.values[i]),
-                  "feh": (df.iso_smet.values[i],
-                          df.iso_smet_err2.values[i]),
-                  "parallax": (df.parallax.values[i],
-                               df.parallax_error.values[i])}
-
-    star = sd.star(iso_params, prot[i], prot_err[i], savedir=savedir,
-                   suffix=str(id).zfill(9))
-    sampler = star.fit()
-    star.make_plots()
+if __name__ == "__main__":
+    full_df = pd.read_csv("kane_cks_tdmra_dr2.csv")
+    p = Pool(4)
+    list(p.map(measure_koi_ages, full_df.iterrows()))
+    # for row in full_df.iterrows():
+    #     measure_koi_ages(row)
